1e0d5dfd5d6b003b402b3462b9349e5cf749ca08
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-02-27 18:53:10 +0100

    created lesson edit page

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,28 @@
+class LessonsController < ApplicationController
+ def destroy
+    @lesson = Lesson.find_by_id(params[:id])
+    @lesson.destroy
+    redirect_to lessons_path, notice: 'The lesson has been deleted'
+  end
+  
+  def edit
+     @lesson = Lesson.find_by_id(params[:id])
+  end 
+  
+  def update
+      @lesson = Lesson.find_by_id(params[:id])
+    
+        if @lesson.update(lesson_params)
+            redirect_to lessons_path, notice: 'You succesfully updated the lesson.'
+        else
+          render :edit
+        end
+   
+  end
+
+    
+  private
+  def lesson_params
+    params.require(:lesson).permit(:date, :timeslot, :spot_id)
+  end
+end
