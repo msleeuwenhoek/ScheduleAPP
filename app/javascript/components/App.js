b0da870ef2ccf0888a49c0ce0515795ae96488a0
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-03-27 16:00:02 +0200

    minor routing changes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -4,22 +4,22 @@ import Spots from "./Spots/Spots";
 import Spot from "./Spots/Spot";
 import Home from "./Home";
 import Dashboard from "./Dashboard";
-import User from "./Users/user";
+import User from "./Users/User";
 import Users from "./Users/Users";
 import Lessons from "./Lessons/Lessons";
-import Lesson from "./Lessons/lesson";
+import Lesson from "./Lessons/Lesson";
 
 export default function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/dashboard" element={<Dashboard />} />
-      <Route exact path="/spots" element={<Spots />} />
-      <Route exact path="/spots/:id" element={<Spot />} />
-      <Route exact path="/lessons" element={<Lessons />} />
-      <Route exact path="/lessons/:id" element={<Lesson />} />
-      <Route exact path="/users" element={<Users />} />
-      <Route exact path="/users/:id" element={<User />} />
+      <Route exact path="/admin/spots" element={<Spots />} />
+      <Route exact path="/admin/spots/:id" element={<Spot />} />
+      <Route exact path="/admin/lessons" element={Lessons} />
+      <Route exact path="/admin/lessons/:id" element={Lesson} />
+      <Route exact path="/admin/users" element={Users} />
+      <Route exact path="/admin/users/:id" element={User} />
     </Routes>
   );
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get '/dashboard', to: 'pages#dashboard'
   
   devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
   namespace :api do
